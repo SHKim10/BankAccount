0b1347bc1026812cfc8f66883d3c8700c6cbb642
Stacy <skim10@stuy.edu>
2020-10-28 03:00:46 -0400

    Finished setPassword and deposit

--- BankAccount.java
+++ BankAccount.java
@@ -15,4 +15,15 @@ public class BankAccount{
   public int getAccountID(){
     return accountID;
   }
+  public void setPassword(String newPass){
+    password = newPass;
+  }
+  public boolean deposit(double amount){
+    if (amount < 0){
+      return false;
+    } else {
+      balance += amount;
+      return true;
+    }
+  }
 }
--- Tester.java
+++ Tester.java
@@ -1,7 +1,16 @@
 public class Tester{
   public static void main (String[] args){
-    BankAccount account = new BankAccount(214817, "mypassword");
-    System.out.println(account.getBalance());      // should return 0.0
-    System.out.println(account.getAccountID());    // should return 95
+    BankAccount test = new BankAccount(214817, "mypassword");
+
+    System.out.println(test.getBalance());      //0.0
+    System.out.println(test.getAccountID());    //214817
+    test.setPassword("secure");                 //"mypassword" changed to "secure"
+
+    System.out.println(test.deposit(-23.7));    //false
+    System.out.println(test.getBalance());      //0.0
+    System.out.println(test.deposit(58));       //true
+    System.out.println(test.getBalance());      //58.0
+    System.out.println(test.deposit(704.2));    //true
+    System.out.println(test.getBalance());      //704.2
   }
 }
